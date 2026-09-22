ac9848e9ab55857865396f379c02988c9e4f03d2
M.Mezni <mamezni@gmail.com>
2026-09-22 18:12:09 -0400

    Add main.py

--- shannon/main.py
+++ shannon/main.py
@@ -1,6 +1,3 @@
-
-
-
 from anthropic import Anthropic
 from dotenv import load_dotenv
 
@@ -10,20 +7,38 @@ client = Anthropic()
 model = "claude-haiku-4-5-20251001"
 
 def add_user_message(messages, text):
-    user_message = {"role":"user", "content":text}
+    user_message = {"role": "user", "content": text}
     messages.append(user_message)
 
-def add_assinstant_message(messages, text):
-    assistant_message = {"role":"assistant", "content":text}
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
     messages.append(assistant_message)
 
-def chat(messages):
-    result = client.messages.create(
-        model=model,
-        max_tokens=1000,
-        messages=messages
-    )
-    print (result.content[0].text)
+def chat(messages, system=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+    }
+
+    if system:
+        params["system"] = system 
+    message = client.messages.create(**params)
+
+    return message.content[0].text
+
+# 1. Define your dynamic variables
+question = "What is quantum computing? Answer in one sentence"
+
+# 2. Use an f-string to inject the variable directly into the template
+prompt = f"""
+Act as an expert. Please answer the user's question:
+
+{question}
+"""
+
 messages = []
-add_user_message(messages,"What is quantum computing? Answer in one sentence")
-chat(messages)
+add_user_message(messages, prompt)
+
+result = chat(messages)
+print(result)
